792a84e2b6bb52e53813f982ba45f40d3297af6b
Cyrus Dadgar <java_siphon@yahoo.com>
2019-11-19 16:55:15 +0000

    fixed logic error in kibana code

--- x-pack/legacy/plugins/maps/public/layers/sources/es_source.js
+++ x-pack/legacy/plugins/maps/public/layers/sources/es_source.js
@@ -243,7 +243,7 @@ export class AbstractESSource extends AbstractVectorSource {
     });
 
     // Do not use field formatters for counting metrics
-    if (metricField && metricField.type === METRIC_TYPE.COUNT || metricField.type === METRIC_TYPE.UNIQUE_COUNT) {
+    if (metricField && (metricField.type === METRIC_TYPE.COUNT || metricField.type === METRIC_TYPE.UNIQUE_COUNT)) {
       return null;
     }
 
